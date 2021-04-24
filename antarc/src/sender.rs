8a2383117fd6fa4541a076c3f38c2f44150193e1
meowjesty <meowjesty_nyan@outlook.com>
2021-04-24 06:28:52 -0300

    Started tackling the prepare to send event.

--- antarc/src/sender.rs
+++ antarc/src/sender.rs
@@ -28,6 +28,7 @@ pub(crate) struct Destination {
 #[derive(Debug, PartialEq, PartialOrd)]
 pub(crate) struct PreparePacketToSend {
     pub(crate) packet_id: Entity,
+    pub(crate) host_id: Entity,
 }
 
 #[derive(Debug, PartialEq, PartialOrd)]
@@ -41,7 +42,96 @@ pub(crate) struct RawPacket {
 pub(crate) struct LatestSent;
 
 pub(crate) fn prepare_packet_to_send(world: &mut World) {
-    for (event_id, (event,)) in world.query::<(&PreparePacketToSend,)>().iter() {}
+    let mut handled_events = Vec::with_capacity(8);
+    let mut send_packets = Vec::with_capacity(8);
+
+    // TODO(alex) 2021-04-24: Steps to do here:
+    // 1. Loop through the enqueued packets event `PreparePacketToSend` and grab the
+    // `Payload + Destination`;
+    //
+    // 2. Look for the `LatestSent` packet to this `Destination`;
+    //      2.1 incrmenet sequence based on the packet sent;
+    //
+    // 3. Look for the `LatestReceived` packet with `Source == Destination`;
+    //      3.1 ack the packet received;
+    //
+    // 4. Generate a `Header` based on these packets;
+    //
+    // 5. Only move this packet into `SendPacket` event if it's time enqueued is greater than the
+    // `LatestSent` time enqueued?
+    for (event_id, (event,)) in world.query::<(&PreparePacketToSend,)>().iter() {
+        // TODO(alex) 2021-04-24: This needs to loop through every packet that must be acked in
+        // order to properly calculate the `past_acks`.
+        let ack = match world
+            .query::<&AckRemotePacket>()
+            .iter()
+            .find(|(ack_event_id, ack_event)| ack_event.host_id == event.host_id)
+        {
+            Some((ack_event_id, ack_event)) => world
+                .query_one::<&Header>(ack_event.packet_id)
+                .unwrap()
+                .get()
+                .unwrap()
+                .sequence
+                .get(),
+            None => 0,
+        };
+
+        let sent_sequence = match world
+            .query::<(&Header, &LatestSent, &Destination)>()
+            .iter()
+            .find_map(|(sent_id, (header, _, destination))| {
+                (destination.host_id == event.host_id).then_some(header.sequence.get())
+            }) {
+            Some(sequence) => sequence,
+            None => {
+                eprintln!("Only connection requests won't have a latest sent?");
+                unreachable!();
+            }
+        };
+
+        let (payload, address) = world
+            .query_one::<(&Payload, &Address)>(event.packet_id)
+            .unwrap()
+            .get()
+            .unwrap();
+
+        let header = Header {
+            sequence: unsafe { Sequence::new_unchecked(sent_sequence + 1) },
+            ack,
+            past_acks: todo!(),
+            status_code: todo!(),
+            // TODO(alex) 2021-04-10: Do not allow packets with large payloads, this will be
+            // properly handled with fragmentation, but right now we could just check before
+            // assignment!
+            payload_length: payload.len() as u16,
+        };
+
+        let connection_id = todo!();
+        // TODO(alex) 2021-04-05: `new_footer` will always exist here, but when we start handling
+        // encoding errors, then it might not be created.
+        let (bytes, footer) = Packet::encode(&header, payload, connection_id).unwrap();
+        let raw_packet = RawPacket {
+            packet_id: event.packet_id,
+            bytes,
+            address: address.clone().0,
+        };
+
+        send_packets.push((event.packet_id, header, footer, raw_packet));
+    }
+
+    while let Some(event_id) = handled_events.pop() {
+        let _ = world.despawn(event_id).unwrap();
+    }
+
+    while let Some((packet_id, header, footer, raw_packet)) = send_packets.pop() {
+        let _ = world.insert(packet_id, (header, footer)).unwrap();
+        let raw_packet_id = world.spawn((raw_packet,));
+
+        let _ = world.spawn((SendPacket {
+            packet_id: raw_packet_id,
+        },));
+    }
 }
 
 pub(crate) fn system_sender(
@@ -52,11 +142,8 @@ pub(crate) fn system_sender(
 ) {
     let mut handled_events = Vec::with_capacity(8);
     let mut sent_packets = Vec::with_capacity(8);
-    let mut new_footer = None;
-    let mut sent = None;
-    let mut old_latest_sent = None;
 
-    if let Some((event_id, _writable)) = world.query::<&Writable>().iter().next() {
+    if let Some((writable_id, _writable)) = world.query::<&Writable>().iter().next() {
         if let Some((_, resource)) = world.query::<&mut NetworkResource>().iter().next() {
             let socket = &mut resource.socket;
 
@@ -72,15 +159,42 @@ pub(crate) fn system_sender(
                     Ok(num_sent) => {
                         debug_assert!(num_sent > 0);
 
-                        sent_packets.push(raw_packet.packet_id);
-                        handled_events.push((event_id, send_event_id));
+                        sent_packets.push((event.packet_id, raw_packet.packet_id));
+                        handled_events.push((writable_id, Some(send_event_id)));
                     }
                     Err(fail) => {
                         eprintln!("Failed to send {:#?} with {:#?}.", raw_packet, fail);
+                        handled_events.push((writable_id, None));
                     }
                 }
             }
         }
+    }
+
+    while let Some((writable_id, send_event_id)) = handled_events.pop() {
+        let _ = world.despawn(writable_id).unwrap();
+
+        if let Some(send_event_id) = send_event_id {
+            let _ = world.despawn(send_event_id).unwrap();
+        }
+    }
+
+    while let Some((raw_id, packet_id)) = sent_packets.pop() {
+        // TODO(alex) 2021-04-24: Just despawning the raw packets after they've been sent, is there
+        // any reason to keep them for longer?
+        let _ = world.despawn(raw_id).unwrap();
+
+        let _ = world
+            .insert(
+                packet_id,
+                (
+                    Sent {
+                        time: timer.elapsed(),
+                    },
+                    LatestSent,
+                ),
+            )
+            .unwrap();
     }
 
     if let Some(packet) = world
@@ -90,26 +204,6 @@ pub(crate) fn system_sender(
         .iter()
         .next()
     {
-        let (packet_id, (payload, destination, address)) = packet;
-
-        let latest_sent_to_remote = world
-            .query::<(&Header, &Destination)>()
-            .with::<Sent>()
-            .with::<LatestSent>()
-            .iter()
-            .find_map(|(packet_id, (header, sent_to))| {
-                (sent_to == destination).then_some((packet_id, header.clone()))
-            });
-
-        // TODO(alex) 2021-04-20: It might be possible to keep an `Option<PacketId>` reference in
-        // the `Host`, and it just gets updated on every successful send, instead of querying like
-        // this.
-        let sequence = latest_sent_to_remote
-            .as_ref()
-            .map_or(unsafe { Sequence::new_unchecked(1) }, |(_, header)| {
-                header.sequence
-            });
-
         // TODO(alex) 2021-04-11: This will ignore some of the packets received, while the socket
         // isn't ready to send, other packets may arrive and take the `LatestReceived` token,
         // leaving some received packets in an ack limbo.
@@ -138,110 +232,62 @@ pub(crate) fn system_sender(
         // ADD(alex) 2021-04-20: There must be a check to grab the `LatestReceived` and ack based on
         // it, and drop the lagged events. It's doing the `LatestReceived` bit, but we're **not**
         // dropping the lagged events.
-        let (event_id, remote_to_ack) = world
-            .query::<(&AckRemotePacket,)>()
-            .iter()
-            .find_map(|(event_id, (event,))| {
-                (event.host_id == destination.host_id).then_some((event_id, packet_id))
-            })
-            .map_or((None, 0), |(event_id, packet_id)| {
-                (
-                    Some(event_id),
-                    world
-                        .query_one::<&Header>(packet_id)
-                        .unwrap()
-                        .with::<LatestReceived>()
-                        .get()
-                        .unwrap()
-                        .sequence
-                        .get(),
-                )
-            });
+        // let ack_diff = {
+        //     let latest_ack = latest_sent_to_remote
+        //         .as_ref()
+        //         .map_or(0, |(_, header)| header.ack);
+        //     let diff = remote_to_ack - latest_ack;
+        //     diff
+        // };
 
-        let ack_diff = {
-            let latest_ack = latest_sent_to_remote
-                .as_ref()
-                .map_or(0, |(_, header)| header.ack);
-            let diff = remote_to_ack - latest_ack;
-            diff
-        };
+        // let past_acks = {
+        //     let latest_past_acks = latest_sent_to_remote
+        //         .as_ref()
+        //         .map_or(0, |(_, header)| header.past_acks);
 
-        let past_acks = {
-            let latest_past_acks = latest_sent_to_remote
-                .as_ref()
-                .map_or(0, |(_, header)| header.past_acks);
+        //     (latest_past_acks << ack_diff) | 0b1
+        // };
 
-            (latest_past_acks << ack_diff) | 0b1
-        };
-
-        let header = Header {
-            sequence,
-            ack: remote_to_ack,
-            past_acks,
-            status_code,
-            // TODO(alex) 2021-04-10: Do not allow packets with large payloads, this will be
-            // properly handled with fragmentation, but right now we could just check before
-            // assignment!
-            payload_length: payload.len() as u16,
-        };
-
-        let connection_id = world
-            .query_one::<&AckingConnection>(destination.host_id)
-            .map_or(None, |mut query| query.get().map(|host| host.connection_id))
-            .or_else(|| {
-                world
-                    .query_one::<&Connected>(destination.host_id)
-                    .map_or(None, |mut query| query.get().map(|host| host.connection_id))
-            });
+        // let connection_id = world
+        //     .query_one::<&AckingConnection>(destination.host_id)
+        //     .map_or(None, |mut query| query.get().map(|host| host.connection_id))
+        //     .or_else(|| {
+        //         world
+        //             .query_one::<&Connected>(destination.host_id)
+        //             .map_or(None, |mut query| query.get().map(|host| host.connection_id))
+        //     });
 
         // TODO(alex) 2021-04-04: This is going to send only 1 packet, we're using `next` on the
         // iterator and not looping! This system is part of a `tick` that has to be constantly
         // called, the looping shouldn't be done here, I think.
         //
-        // TODO(alex) 2021-04-05: `new_footer` will always exist here, but when we start handling
-        // encoding errors, then it might not be created.
-        let (packet_bytes, footer) = Packet::encode(&header, payload, connection_id).unwrap();
-        new_footer = Some((packet_id, (footer,)));
-
-        match socket.send_to(&packet_bytes, address.0) {
-            Ok(num_sent) => {
-                debug_assert!(num_sent > 0);
-                sent = Some((packet_id, destination.host_id));
-                old_latest_sent = latest_sent_to_remote;
-                // handled_events.push(event_id);
-            }
-            Err(fail) => {
-                eprintln!("Failed to send packet with {:#?}.", fail);
-                todo!()
-            }
-        }
     }
 
     // while let Some(event_id) = handled_events.pop().flatten() {
     //     let _ = world.despawn(event_id).unwrap();
     // }
 
-    if let Some((packet_id, (footer,))) = new_footer {
-        world.insert(packet_id, (footer,)).unwrap();
-    }
+    // if let Some((packet_id, (footer,))) = new_footer {
+    //     world.insert(packet_id, (footer,)).unwrap();
+    // }
 
-    if let Some((packet_id, host_id)) = sent {
-        world
-            .insert(
-                packet_id,
-                (Sent {
-                    time: timer.elapsed(),
-                },),
-            )
-            .unwrap();
+    // if let Some((packet_id, host_id)) = sent {
+    //     world
+    //         .insert(
+    //             packet_id,
+    //             (Sent {
+    //                 time: timer.elapsed(),
+    //             },),
+    //         )
+    //         .unwrap();
 
-        // let mut host = world.get_mut::<Host>(host_id).unwrap();
-        // host.sequence_tracker = Sequence::new(host.sequence_tracker.get() + 1).unwrap();
-    }
+    //     // let mut host = world.get_mut::<Host>(host_id).unwrap();
+    //     // host.sequence_tracker = Sequence::new(host.sequence_tracker.get() + 1).unwrap();
+    // }
 
-    if let Some((packet_id, _)) = old_latest_sent {
-        let _ = world.remove::<(LatestSent,)>(packet_id).unwrap();
-    }
+    // if let Some((packet_id, _)) = old_latest_sent {
+    //     let _ = world.remove::<(LatestSent,)>(packet_id).unwrap();
+    // }
 }
 
 // TODO(alex) 2021-04-05: We could use a `NeedsUpdate` or some sort of `Changed` indicator for a
